947a28fdee4cd682e300bfefec4d1dc997f3f51b
James Kienle <james.kienle@gmail.com>
2024-05-15 21:22:38 -0700

    First attempt at adding events

--- crates/manatee/src/window.rs
+++ crates/manatee/src/window.rs
@@ -1,8 +1,13 @@
-use winit::{application::ApplicationHandler, event::WindowEvent, event_loop::{ActiveEventLoop, EventLoop, EventLoopProxy}, window::{Window, WindowId}};
+use winit::{application::ApplicationHandler, event::WindowEvent, event_loop::{ActiveEventLoop, EventLoop, EventLoopProxy}, raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle}, window::{Window, WindowId}};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 pub enum ManateeWindowEvent {
-    CreateMainWindow
+    CreateWindow {
+        title: String
+    },
+    SetMainWindowParams {
+        title: Option<String>
+    }
 }
 
 #[derive(Default)]
@@ -20,8 +25,22 @@ impl ApplicationHandler::<ManateeWindowEvent> for WinitApp {
         );
     }
     fn user_event(&mut self, event_loop: &ActiveEventLoop, event: ManateeWindowEvent) {
-        println!("User event: {event:?}");
-        let _ = event_loop.create_window(Window::default_attributes());
+        match event {
+            ManateeWindowEvent::CreateWindow { title } => {
+                let parent_window = self.main_window.as_ref().unwrap().raw_window_handle().unwrap();
+                let mut child_window_attributes = Window::default_attributes().with_title(title);
+                child_window_attributes = unsafe { child_window_attributes.with_parent_window(Some(parent_window)) };
+                // child_window_attributes = unsafe child_window_attributes.with_parent_window((Some(self.main_window.unwrap().raw_window_handle())));
+                let _ = event_loop.create_window(child_window_attributes);
+            },
+            ManateeWindowEvent::SetMainWindowParams { title } => {
+                if (Option::is_some(&title)) {
+                    self.main_window.as_ref().unwrap().set_title(title.unwrap().as_str())
+                }
+            },
+            _ => (),
+        }
+        // let _ = event_loop.create_window(Window::default_attributes());
     }
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         // This will probably need to be a match after the app grows a bit
@@ -37,14 +56,14 @@ pub struct WindowEventLoop {
 }
 
 pub trait WindowEventLoopManager {
-    fn create_window(&mut self) {}
+    fn create_window(&mut self, _title: String) {}
     fn send_event(&mut self, _event: ManateeWindowEvent) {}
     fn start_event_loop(&mut self) {}
 }
 
 impl WindowEventLoopManager for WindowEventLoop {
-    fn create_window(&mut self) {
-        self.send_event(ManateeWindowEvent::CreateMainWindow)
+    fn create_window(&mut self, title: String) {
+        self.send_event(ManateeWindowEvent::CreateWindow { title })
     }
     fn send_event(&mut self, event: ManateeWindowEvent) {
         let _ = self.event_loop_proxy.as_ref().unwrap().send_event(event);
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 #![windows_subsystem = "windows"]
+use std::str::FromStr;
+
 pub use manatee::window::{WindowEventLoop, WindowEventLoopManager};
 
 pub fn main() {
     let mut window_event_loop = WindowEventLoop::default();
     window_event_loop.start_event_loop();
-    // window_event_loop.create_window();
+    window_event_loop.create_window("Foo".to_string());
 }
