2fd3b9a6795f803e88140e3eddce0907e7b9478a
James Kienle <james.kienle@gmail.com>
2024-05-27 00:55:27 -0700

    HOLY FUCKING SHIT I GOT PIXELS RENDERING ON THE SCREEN

--- crates/manatee/src/game.rs
+++ crates/manatee/src/game.rs
@@ -25,14 +25,52 @@ impl ApplicationHandler for GameWindowState {
         );
         println!("Window Created");
 
-        let gpu = Arc::new(Gpu::new(Arc::clone(&window)));
+        let gpu = Arc::new(pollster::block_on(Gpu::new(window.clone())));
         println!("GPU Created");
 
         self.window = Some(window);
         println!("Window Set");
 
         self.gpu = Some(gpu);
-        println!("GPU Set")
+        println!("GPU Set");
+
+        let device = self.gpu.as_ref().unwrap().device();
+        let queue = self.gpu.as_ref().unwrap().queue();
+        let surface = self.gpu.as_ref().unwrap().surface();
+
+        let output = surface.get_current_texture().unwrap();
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+        println!("Current Texture Found");
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Render Encoder"),
+        });
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+        }
+    
+        // submit will accept anything that implements IntoIter
+        queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+        println!("Rendered");
     }
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         // This will probably need to be a match after the app grows a bit
--- crates/manatee/src/gpu.rs
+++ crates/manatee/src/gpu.rs
@@ -1,17 +1,44 @@
 use std::sync::Arc;
-use wgpu::Surface;
+use wgpu::{Device,Queue,Surface};
 use winit::window::Window;
 
 pub struct Gpu {
+    device: Device,
+    queue: Queue,
     surface: Surface<'static>,
 }
 
 impl Gpu {
-    pub fn new(window: Arc<Window>) -> Self {
-        let instance = wgpu::Instance::default();
-        let size = window.inner_size();
-        let surface = instance.create_surface(window).unwrap();
+    pub async fn new(window: Arc<Window>) -> Self {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::default());
+        let surface = instance.create_surface(window.clone()).unwrap();
+        let adapter = instance.request_adapter(&wgpu::RequestAdapterOptionsBase::default()).await
+        .expect("Invalid Graphics Backend!");
 
-        Self { surface }
+        let (device, queue) = adapter
+        .request_device(&wgpu::DeviceDescriptor::default(), None)
+        .await
+        .unwrap();
+
+        // TODO: Move this to resize method
+        let config = surface.get_default_config(&adapter, window.inner_size().width, window.inner_size().height).unwrap();
+        surface.configure(&device, &config);
+
+        Self { device, queue, surface }
+    }
+
+    pub fn render_frame() {}
+
+    pub fn device(&self) -> &Device {
+        &self.device
+    }
+
+    pub fn queue(&self) -> &Queue {
+        &self.queue
+    }
+
+    pub fn surface(&self) -> &Surface<'static> {
+        &self.surface
     }
 }
+ 
