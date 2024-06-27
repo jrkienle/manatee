2f7ee9c4ab6ae8d92cae6cb2dc1346f27b85a39f
James Kienle <james.kienle@gmail.com>
2024-06-26 21:02:03 -0700

    Begin Phasing Out Winit (TODO: Add Wgpu Back)

--- crates/manatee/src/game.rs
+++ crates/manatee/src/game.rs
@@ -1,13 +1,7 @@
-use std::sync::Arc;
-
-use winit::{event_loop::EventLoop, window::Window};
-
-use crate::{gpu::Gpu, window_state::WindowState};
+use aquarium::{App, WindowParams};
 
 pub struct Game {
-    event_loop: EventLoop<()>,
-    gpu: Option<Arc<Gpu>>,
-    window: Option<Arc<Window>>,
+    app: App,
 }
 
 impl Default for Game {
@@ -18,17 +12,17 @@ impl Default for Game {
 
 impl Game {
     pub fn new() -> Self {
-        let event_loop = EventLoop::new().unwrap();
-        Self {
-            event_loop,
-            gpu: None,
-            window: None,
-        }
+        let app = App::new();
+        Self { app }
     }
 
     pub fn run(self) {
         println!("Running your game");
-        let mut window_state = WindowState::new(self.window, self.gpu);
-        self.event_loop.run_app(&mut window_state).unwrap();
+        self.app.run(|app_ctx| {
+            app_ctx.new_window(WindowParams {
+                title: "Manatee Game",
+                ..Default::default()
+            });
+        })
     }
 }
