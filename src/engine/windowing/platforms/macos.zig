0674e8af44405952d8580d9eeab95c87af58ab0b
James Kienle <james.kienle@gmail.com>
2024-07-07 13:13:34 -0700

    Update MacOS Messages

--- src/engine/windowing/platforms/macos.zig
+++ src/engine/windowing/platforms/macos.zig
@@ -2,9 +2,15 @@ const std = @import("std");
 
 pub fn init() void {
     // TODO: Implement MacOS Platform
-    std.debug.print("TODO: Implement Platform.init() for MacOS", .{});
+    std.debug.print("TODO: Implement Platform.init() for MacOS\n", .{});
 }
 
+pub const Window = struct {
+    pub fn init() void {
+        std.debug.print("TODO: Implement Platform.Window.init() for MacOS\n", .{});
+    }
+};
+
 pub fn run() void {
-    std.debug.print("TODO: Implement Platform.run() for MacOS", .{});
+    std.debug.print("TODO: Implement Platform.run() for MacOS\n", .{});
 }
