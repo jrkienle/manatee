88e7d03d5856f1f3cd9aa7fb0ec322e37d7a8aac
James Kienle <james.kienle@gmail.com>
2024-07-12 22:44:13 -0700

    Temporarily remove macos windowing placeholder

--- src/engine/windowing/windowing.zig
+++ src/engine/windowing/windowing.zig
@@ -2,7 +2,9 @@ const builtin = @import("builtin");
 const std = @import("std");
 
 const platform = switch (builtin.os.tag) {
-    .macos => @import("./platforms/macos.zig"),
+    // I'm not getting working autocomplete on windows with this plugged in, gotta love using an
+    // alpha release language with no official tooling!
+    // .macos => @import("./platforms/macos.zig"),
     .windows => @import("./platforms/windows.zig"),
     else => @compileError(std.fmt.comptimePrint("Unsupported OS: {}", .{builtin.os.tag})),
 };
